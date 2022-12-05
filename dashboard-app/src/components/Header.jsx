b704b16f2840928f3ca201a61406469f6aa7ba63
Theologos Mavropoulos <rusinner88@gmail.com>
2022-12-05 15:02:52 +0200

    orders

--- dashboard-app/src/components/Header.jsx
+++ dashboard-app/src/components/Header.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-const Header = () => {
-  return <div></div>;
+const Header = ({ category, title }) => {
+  return (
+    <div className="mb-10">
+      <p className="tetx-gray-400">{category}</p>
+      <p className="tetx-3xl font-extrabold tracking-tight text-slate-900">
+        {title}
+      </p>
+    </div>
+  );
 };
 
 export default Header;
