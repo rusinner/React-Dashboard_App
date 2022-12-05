4c02bb4c7f92e6ca3232e1e728c319e6beaebe08
Theologos Mavropoulos <rusinner88@gmail.com>
2022-12-05 11:51:59 +0200

    Button in ecommerce

--- dashboard-app/src/pages/Ecommerce.jsx
+++ dashboard-app/src/pages/Ecommerce.jsx
@@ -13,7 +13,22 @@ const Ecommerce = () => {
   return (
     <div className="mt-12">
       <div className="flex flex-wrap lg:flex-nowrap justify-center">
-        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-contain bg-center"></div>
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-contain bg-center">
+          <div className="flex justify-between items-center">
+            <div>
+              <p className="font-bold text-gray-400">Earnings</p>
+              <p className="tetx-2xl">45,876.23 €</p>
+            </div>
+          </div>
+          <div className="mt-6">
+            <Button
+              color="white"
+              bgColor="blue"
+              text="download"
+              borderRadius="10px"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
