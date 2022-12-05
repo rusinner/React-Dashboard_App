956583cfb1b12fe79ec01b1896034130b0b545d9
Theologos Mavropoulos <rusinner88@gmail.com>
2022-12-05 14:29:55 +0200

    data tables

--- dashboard-app/src/pages/Ecommerce.jsx
+++ dashboard-app/src/pages/Ecommerce.jsx
@@ -94,6 +94,28 @@ const Ecommerce = () => {
                 </p>
                 <p className="text-gray-500 mt-1">Expense</p>
               </div>
+              <div className="mt-5">
+                <SparkLine
+                  currentColor="blue"
+                  id="line-sparkLine"
+                  type="Line"
+                  height="80px"
+                  width="250px"
+                  data={SparklineAreaData}
+                  color="blue"
+                />
+              </div>
+              <div className="mt-10">
+                <Button
+                  color="white"
+                  bgColor="blue"
+                  text="Download Report"
+                  borderRadius="10px"
+                />
+              </div>
+            </div>
+            <div>
+              <Stacked width="320px" height="360px" />
             </div>
           </div>
         </div>
