01b38b193a732864540d8590e81e4d6f62cacbb2
Theologos Mavropoulos <rusinner88@gmail.com>
2022-12-07 23:45:10 +0200

    final

--- dashboard-app/src/components/Navbar.jsx
+++ dashboard-app/src/components/Navbar.jsx
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
 import { FiShoppingCart } from "react-icons/fi";
 import { BsChatLeft } from "react-icons/bs";
-import { BsChatRight } from "react-icons/bs";
 import { RiNotification3Line } from "react-icons/ri";
 import { MdKeyboardArrowDown } from "react-icons/md";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
