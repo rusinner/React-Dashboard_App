5bb20ddbd86a800b672118a559c719ab018c8787
Theologos Mavropoulos <rusinner88@gmail.com>
2022-12-07 23:33:05 +0200

    close button functionality

--- dashboard-app/src/components/Cart.jsx
+++ dashboard-app/src/components/Cart.jsx
@@ -7,20 +7,20 @@ import { cartData } from "../data/dummy";
 import { Button } from ".";
 
 const Cart = () => {
-  const { currentColor } = useStateContext();
+  const { currentColor, setIsClicked } = useStateContext();
 
   return (
     <div className="bg-half-transparent w-full fixed nav-item top-0 right-0 ">
       <div className="float-right h-screen  duration-1000 ease-in-out dark:text-gray-200 transition-all dark:bg-[#484B52] bg-white md:w-400 p-8">
         <div className="flex justify-between items-center">
           <p className="font-semibold text-lg">Shopping Cart</p>
-          <Button
-            icon={<MdOutlineCancel />}
-            color="rgb(153, 171, 180)"
-            bgHoverColor="light-gray"
-            size="2xl"
-            borderRadius="50%"
-          />
+          <button
+            type="button"
+            className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block "
+            onClick={() => setIsClicked((isClicked) => !isClicked)}
+          >
+            <MdOutlineCancel />
+          </button>
         </div>
         {cartData?.map((item, index) => (
           <div key={index}>
